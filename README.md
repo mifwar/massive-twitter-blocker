ffdc24e03a492b9dd444ef823b841bd15a5fabaa
Miftahul Anwar <miftahulanwar@ce.student.pens.ac.id>
2023-04-06 11:15:23 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # massive-twitter-blocker
+
+## Description
+
 A Python script that automates blocking multiple Twitter users at once
+
+## Dependencies
+
+- Python 3.6+
+- Selenium
+
+## Installation
+
+1. Clone this repository: `git clone https://github.com/your-username/twitter-blocker.git`
+2. Install the dependencies: `pip install -r requirements.txt`
+
+## Usage
+
+## Usage
+
+1. Clone this repository to your local machine using `git clone https://github.com/mifwar/massive-twitter-blocker.git`.
+2. Modify the `personal.txt` file and fill it with your Twitter username and password, separated by a new line.
+3. Modify the `accounts.txt` file and add the Twitter usernames of the users you want to block, one per line.
+4. Open a terminal window and navigate to the root directory of the project.
+5. Run the script using the command: `python main.py`
+6. Watch as the script navigates to each user's profile, blocks them, and moves on to the next user.
+
+Note: If a user is already blocked, the script will skip them and move on to the next user.
+
+## Contributing
+
+1. Fork it (https://github.com/mifwar/massive-twitter-blocker/fork)
+2. Create your feature branch (`git checkout -b feature/your-feature-name`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin feature/your-feature-name`)
+5. Create a new Pull Request
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
---
+++ main.py
@@ -0,0 +1,104 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+def read_credentials(file_path):
+    with open(file_path, 'r') as file:
+        username, password = [line.strip() for line in file.readlines()]
+    return username, password
+
+def login_twitter(driver, username, password):
+    driver.get('https://twitter.com/login')
+
+    try:
+        username_field = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//span[text()="Phone, email, or username"]/ancestor::label//input'))
+        )
+        next_button = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '//span[text()="Next"]/ancestor::div[@role="button"]'))
+        )
+
+        username_field.send_keys(username)
+        next_button.click()
+        time.sleep(2)
+
+        password_field = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//input[@name="password"]'))
+        )
+        login_button = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '//div[@data-testid="LoginForm_Login_Button"]'))
+        )
+
+        password_field.send_keys(password)
+        login_button.click()
+        time.sleep(2)
+
+    except TimeoutException:
+        print("Timeout: unable to locate login elements.")
+
+def block_user(driver, user_to_block):
+    # Navigate to the user's profile page
+    driver.get(f'https://twitter.com/{user_to_block}')
+
+    try:
+        # Check if the user is already blocked
+        blocked_button = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[@data-testid='1327952050320097280-unblock']"))
+        )
+        print(f"User {user_to_block} is already blocked.")
+        return
+
+    except TimeoutException:
+        # User is not blocked, continue with blocking process
+        pass
+
+    try:
+        # Click on the three-dot menu button
+        menu_button = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[@data-testid='userActions']"))
+        )
+        menu_button.click()
+
+        # Find the 'Block' button in the dropdown menu
+        block_button = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[@role='menuitem'][@data-testid='block']"))
+        )
+        block_button.click()
+
+        # Confirm the block action in the modal
+        confirm_button = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[@role='button'][@data-testid='confirmationSheetConfirm']"))
+        )
+        confirm_button.click()
+
+        print(f"User {user_to_block} has been successfully blocked.")
+
+    except NoSuchElementException as e:
+        print(f"Unable to block user {user_to_block}: {e}")
+
+    except TimeoutException:
+        print(f"Unable to block user {user_to_block}: Block button not found.")
+
+def block_users_from_list(file_path):
+    with open(file_path, 'r') as file:
+        users_to_block = [line.strip() for line in file.readlines()]
+
+    driver = webdriver.Chrome()  # Replace with your preferred browser, e.g., webdriver.Firefox()
+
+    username, password = read_credentials('personal.txt')
+    login_twitter(driver, username, password)
+
+    for user in users_to_block:
+        block_user(driver, user)
+
+    driver.quit()
+
+if __name__ == '__main__':
+    # Replace 'user_list.txt' with the path to your txt file containing the usernames to block
+    user_list_path = 'accounts.txt'
+    block_users_from_list(user_list_path)
